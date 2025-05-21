d6e28f820ecf26449ed707005be908319bc21b6b
Wilson <wilson.adjei@etu.unistra.fr>
2025-05-21 14:32:36 +0200

    Ajout des structures nécéssaires dans réseaux.h

---
+++ reseaux.h
@@ -0,0 +1,52 @@
+#pragma once 
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include  <adresse.h>
+
+
+typedef struct {
+    mac_addr_t mac;
+    ip_addr_t ip;
+} Station;
+
+typedef struct{
+    uint8_t port;
+    mac_addr_t adresse_mac;
+
+}Commutation;
+
+
+typedef struct{
+    mac_addr_t mac;
+    uint8_t nb_port;
+    int priorite; 
+    Commutation* tableCommu;
+}Switch;
+
+typedef enum {
+    STATION_TYPE,
+    SWITCH_TYPE
+} EquipementType;
+
+typedef struct {
+    EquipementType type;
+    union {
+        Station station;
+        Switch sw;
+    };
+} Equipement;
+
+
+
+typedef struct {
+    int nb_equipements;
+    Equipement *equipements;
+    int** matrice_adjacence;
+} Graphe;
+
+
+
+
+
+
