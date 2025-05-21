269e4ff4e05cae011ad2af286cae71a55fbdc7af
Stoll-Geyer Yann <stollgeyer.yann@gmail.com>
2025-05-21 15:16:10 +0200

    fonction d'affichage de IP et MAC

--- adresse.c
+++ adresse.c
@@ -1,1 +1,21 @@
 #include "adresse.h"
+
+void affiche_ip(ip_addr_t ip)
+{
+    printf("%d",*ip);
+    for (size_t i = 1; i < 4; i++)
+    {
+        printf(".");
+        printf("%d",*(ip + i));
+    }
+}
+
+void affiche_mac(mac_addr_t mac)
+{
+    printf("%X",*mac);
+    for (size_t i = 1; i < 6; i++)
+    {
+        printf(":");
+        printf("%X",*(mac + i));
+    }
+}
--- adresse.h
+++ adresse.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdio.h>
 
 //Adresse MAC
 typedef uint8_t mac_addr_t[6];
 //Adresse IP
 typedef uint8_t ip_addr_t[4];
 
+void affiche_ip(ip_addr_t);
+void affiche_mac(mac_addr_t);
