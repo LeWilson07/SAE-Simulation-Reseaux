1113c14bd83f82643549e651ff138db544ca388f
ZOU <vzou@unistra.fr>
2025-05-21 14:24:07 +0200

    ajout stucture trame.h

---
+++ trame.h
@@ -0,0 +1,17 @@
+#pragma once 
+#include <stdio.h>
+#include <stdint.h>
+#include <adresse.h>
+#include <reseaux.h>
+
+
+// Structure de la tram
+typedef struct tram{
+    uint8_t preambule[7];
+    mac_addr_t dest;
+    mac_addr_t src;
+    uint8_t type[2];
+    uint8_t *data;
+    uint8_t fcs[4];
+    uint16_t tailleData;
+}tram;
